30e958d5873ef4ecf8357dcd7a6df7aac16ebdee
Luke Park <143543147+lukep258@users.noreply.github.com>
2023-11-15 18:01:10 -0600

    initial commit

--- server.js
+++ server.js
@@ -4,3 +4,31 @@ import pg from 'pg'
 
 const app = express()
 const port = process.env.port
+app.use(cors())
+app.use(express.json())
+
+const init=()=>{
+    app.use((req,res,next)=>{//test request console
+        console.log(req.method,req.body,req.url)
+        res.send('good test')
+    })
+
+    app.use(express.static('public'))
+    app.listen(port,()=>{console.log(`listening on ${port}`)})
+}
+
+const newPool=()=>{
+    const connectionString=process.env.DATABASE_URL||{
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
+    }
+
+    const pool=new pg.Pool({connectionString})
+    return pool
+}
+
+const pool = newPool
+init()
